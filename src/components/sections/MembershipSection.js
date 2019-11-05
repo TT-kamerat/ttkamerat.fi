c9bd058cd1e6c01416de8e3fe45455431875d706
Antti <antti.kallonen@lauta.fi>
2019-11-05 08:35:10 +0200

    Updated payment information

--- src/components/sections/MembershipSection.js
+++ src/components/sections/MembershipSection.js
@@ -17,14 +17,56 @@ class MembershipSection extends Component {
 
     const content = {
       [language.ENGLISH]: <div>
-      <p>Our annual club membership fee is (academic year) is 5 euros which you can pay to the club's bank account:<br/> 
-      <b>{bankAccount}</b> Reference number: <b>1012</b> (Ålandsbanken)</p>
+      <p>Our annual club membership fee is (academic year) is 5 euros which you can pay to the club's bank account:</p>
+      <p>
+      <table className='bank-info'>
+        <tbody>
+          <tr>
+            <td className='bank-info-description'>Receiver</td>
+            <td className='bank-info-data'>Tampereen teekkarikamerat ry</td>
+          </tr>
+          <tr>
+            <td className='bank-info-description'>Account number</td>
+            <td className='bank-info-data'>{bankAccount}</td>
+          </tr>
+          <tr>
+            <td className='bank-info-description'>Reference number</td>
+            <td className='bank-info-data'>1012</td>
+          </tr>
+          <tr>
+            <td className='bank-info-description'>Bank</td>
+            <td className='bank-info-data'>Ålandsbanken</td>
+          </tr>
+      </tbody>
+      </table>
+      </p>
       <p>As our member club's premises and equipment (after signing the reservation agreement) are available for you. After paying the membership fee contact a board member for reclaiming your membership card. With your membership card you can prove your membership when borrowing the clubroom key. According to your will and capabilities you can participate in the improving of our activities.</p>
       <p>Download info bulletin for new members <a href={newMember}>here!</a></p>
       </div>,
       [language.FINNISH]: <div>
-      <p>Kerhomme vuosimaksu (lukuvuosi) on 5 euroa, jonka voit maksaa tilille:<br/> 
-      <b>{bankAccount}</b> Viite: <b>1012</b> (Ålandsbanken)</p>
+      <p>Kerhomme vuosimaksu (lukuvuosi) on 5 euroa, jonka voit maksaa tilille:<br/></p>
+      <p>
+      <table className='bank-info'>
+        <tbody>
+          <tr>
+            <td className='bank-info-description'>Saaja</td>
+            <td className='bank-info-data'>Tampereen teekkarikamerat ry</td>
+          </tr>
+          <tr>
+            <td className='bank-info-description'>Tilinumero</td>
+            <td className='bank-info-data'>{bankAccount}</td>
+          </tr>
+          <tr>
+            <td className='bank-info-description'>Viitenumero</td>
+            <td className='bank-info-data'>1012</td>
+          </tr>
+          <tr>
+            <td className='bank-info-description'>Pankki</td>
+            <td className='bank-info-data'>Ålandsbanken</td>
+          </tr>
+      </tbody>
+      </table>
+      </p>
       <p>Jäsenenä sinulla on kaikki tilamme ja kalustomme (lainaussopimuksen allekirjoitettua) käytettävissäsi. Jäsenmaksun maksettuasi ota yhteyttä johonkin hallituksen jäseneen, sovi hänen kanssaan milloin voit noutaa jäsenkortin, jolla voit todistaa jäsenyytesi pimiön avainta lainatessa. Halutessasi (ja suotavasti) voit myös omalla aktiivisuudellasi olla mukana kehittämässä toimintaamme.</p>
       <p>Lataa <a href={newMember}>tästä</a> uuden jäsenen tietopaketti!</p>
       </div>
